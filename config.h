d1cf48ceacec84f12125760bd71a682a27cbc2e3
Henry Till <henrytill@gmail.com>
2018-04-08 12:52:44 -0400

    update config.h

--- config.h
+++ config.h
@@ -4,9 +4,9 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Mono:pixelsize=12" };
-static const char dmenufont[]       = "Fira Mono:pixelsize=12";
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "LiberationMono:pixelsize=12" };
+static const char dmenufont[]       = "LiberationMono:pixelsize=12";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -56,7 +56,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-b", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xterm", NULL };
 
 static Key keys[] = {
